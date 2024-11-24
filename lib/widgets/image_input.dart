80815e626ff6130aa3c0ee653ade9e70c5d51580
uritrejo <uritrejo20@gmail.com>
2024-11-24 16:58:32 +0100

    can now take & display picture

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,10 +10,48 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    // here you can see the same type of thing as with js promises
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    _selectedImage = File(pickedImage.path);
+
+    // reload the UI for the image to show up
+    setState(() {});
+
+    print("??++ picture taken");
+  }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+        icon: const Icon(Icons.camera),
+        label: const Text('Take Picture'),
+        onPressed: _takePicture);
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        // the ! at the end is cuz you know it'll never be null
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          // make it take over the whole container
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.center,
+        ),
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
         // border: Border.all(width: 1, color: Colors.grey),
@@ -24,10 +64,7 @@ class _ImageInputState extends State<ImageInput> {
       alignment: Alignment
           .center, // center the child in this container (horizontal & vertical)
       // the child will be either a button or the image preview
-      child: TextButton.icon(
-          icon: const Icon(Icons.camera),
-          label: const Text('Take Picture'),
-          onPressed: _takePicture),
+      child: content,
     );
   }
 }
